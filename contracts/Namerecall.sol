1d24b57d70441729f0241c8bf89564647a13a8d8
iwhite1onestone <iwhite@onlyonestone.com>
2023-08-30 15:34:19 -0500

    TestCommit

---
+++ contracts/Namerecall.sol
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.8.2 <0.9.0;
+
+/**
+ * @title Storage
+ * @dev Store & retrieve value in a variable
+ * @custom:dev-run-script ./scripts/deploy_with_ethers.ts
+ */
+contract Storage {
+
+    string public storage_;
+
+    function store(string memory s) public {
+        storage_ = s;
+    }
+
+    /**
+     * @dev Return value 
+     * @return value of 'number'
+     */
+    function retrieve() public view returns (string memory){
+        return storage_;
+    }
+
+    function getName() public view returns (string memory){
+        return "Ian";
+    }
+}
